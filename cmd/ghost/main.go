51e6c15dab400086dd7e1f57c64dcc3b47fb246a
Rojin Dumre <rojin.woeser@gmail.com>
2024-09-14 21:40:04 +1000

    started with the main file. need to edit the import for namespaces in go.mod because its importing from the previous repo i deleted, and its in the cache. So need to remove it from the go.mod file and start fresh with a new import of the current repo package

--- cmd/ghost/main.go
+++ cmd/ghost/main.go
@@ -1,23 +1,52 @@
 package main
 
 import (
-    "log"
-    "os"
+	"flag"
+	"os"
 
-    "github.com/OneThing98/Ghost/pkg/container"
+	container "github.com/OneThing98/containerpkg"
 )
 
-func main() {
-    if len(os.Args) < 3 {
-        log.Fatal("Usage: run <command> or child <command>")
-    }
+var (
+	displayPid bool
+	newCommand string
+	userNet    bool
+)
 
-    runner, err := container.NewContainerRunner(os.Args[1])
-    if err != nil {
-        log.Fatalf("Error creating container runner: %v", err)
-    }
-
-    if err := runner.Run(os.Args[2:]); err != nil {
-        log.Fatalf("Error running command: %v", err)
-    }
+func init() {
+	flag.BoolVar(&displayPid, "pid", false, "display the pid before waiting")
+	flag.StringVar(&newCommand, "cmd", "/bin/bash", "command to run in the existing namespace")
+	flag.BoolVar(&userNet, "net", false, "use a net namespace")
+	flag.Parse()
 }
+
+func exec(container *container.Container) error {
+	var (
+		netFile *os.File
+		err     error
+	)
+	container.NetNsFd = 0
+	if userNet {
+		netFile, err = os.Open("/root/nsroot/test")
+		if err != nil {
+			return err
+		}
+		container.NetNsFd = netFile.Fd()
+	}
+
+}
+
+// func main() {
+//     if len(os.Args) < 3 {
+//         log.Fatal("Usage: run <command> or child <command>")
+//     }
+
+//     runner, err := container.NewContainerRunner(os.Args[1])
+//     if err != nil {
+//         log.Fatalf("Error creating container runner: %v", err)
+//     }
+
+//     if err := runner.Run(os.Args[2:]); err != nil {
+//         log.Fatalf("Error running command: %v", err)
+//     }
+// }
